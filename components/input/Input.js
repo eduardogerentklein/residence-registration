b038daa589162bb7b0e530283cb4467d61fdac9d
Eduardo Klein <eduardo.klein@fulltechpro.com.br>
2021-09-17 21:54:11 -0300

    Adding state to index page

--- components/input/Input.js
+++ components/input/Input.js
@@ -1,22 +1,24 @@
 const Input = ({
-  handleChange,
+  onChange,
   placeholder,
+  name,
   disabled = false,
   type = 'text',
   className = ''
 }) => {
-  const onChange = e => {
-    if (handleChange) handleChange(e.target.value)
+  const handleChange = e => {
+    if (onChange) onChange(e.target)
   }
 
   return (
     <>
       <input
+        name={name}
         disabled={disabled}
         type={type}
         placeholder={placeholder}
         className={`text-3xl border border-black-25 focus:outline-none focus:ring focus:border-primary-hover rounded rounded-md py-2 px-3 ${className}`}
-        onChange={onChange}
+        onChange={handleChange}
       />
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Head from 'next/head'
 
 import Box from '../components/container/Box'
@@ -8,72 +10,94 @@ import Subtitle from '../components/typography/Subtitle'
 import TextButton from '../components/typography/TextButton'
 
 const Index = () => {
+  const [model, setModel] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+  const handleSubmit = () => {
+    setIsLoading(true)
+  }
+
+  const handleChange = e => {
+    setModel(Object.assign(model, { [e.name]: e.value }))
+  }
+
   return (
     <>
       <Head>
         <title>Residence Registration</title>
       </Head>
       <Box>
-        <Subtitle>Residence Registration</Subtitle>
-        <form className='space-y-7'>
-          <div className='flex flex-col space-y-1 mt-7'>
-            <Label labelFor='CEP'>CEP</Label>
-            <Input
-              type='text'
-              name='CEP'
-              id='CEP'
-              placeholder='Ex: 88015-420'
-              className='text-black-75'
-            />
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <Label labelFor='Number'>Number</Label>
-            <Input
-              type='text'
-              name='Number'
-              id='Number'
-              placeholder='Ex: 810'
-              className='text-black-75'
-            />
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <Label labelFor='Number'>Latitude</Label>
-            <Input
-              type='text'
-              name='Latitude'
-              id='Latitude'
-              placeholder='Ex: -48.5457307'
-              className='text-black-75'
-            />
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <Label labelFor='Number'>Longitude</Label>
-            <Input
-              type='text'
-              name='Longitude'
-              id='Longitude'
-              placeholder='Ex: -27.59022'
-              className='text-black-75'
-            />
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <Label labelFor='Number'>Number of residents</Label>
-            <Input
-              type='text'
-              name='residents'
-              id='residents'
-              placeholder='Ex: 4'
-              className='text-black-75'
-            />
-          </div>
-          <div>
-            <Button className='bg-primary rounded rounded-md hover:bg-primary-hover w-full p-2'>
-              <TextButton size={1} className='text-white-75'>
-                Save
-              </TextButton>
-            </Button>
-          </div>
-        </form>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <Subtitle>Residence Registration</Subtitle>
+            <form className='space-y-7' onSubmit={handleSubmit}>
+              <div className='flex flex-col space-y-1 mt-7'>
+                <Label labelFor='CEP'>CEP</Label>
+                <Input
+                  type='text'
+                  name='CEP'
+                  placeholder='Ex: 88015-420'
+                  className='text-black-75'
+                  onChange={handleChange}
+                  value={model.CEP}
+                />
+              </div>
+              <div className='flex flex-col space-y-1'>
+                <Label labelFor='Number'>Number</Label>
+                <Input
+                  type='text'
+                  name='Number'
+                  placeholder='Ex: 810'
+                  className='text-black-75'
+                  onChange={handleChange}
+                  value={model.Number}
+                />
+              </div>
+              <div className='flex flex-col space-y-1'>
+                <Label labelFor='Number'>Latitude</Label>
+                <Input
+                  type='text'
+                  name='Latitude'
+                  placeholder='Ex: -48.5457307'
+                  className='text-black-75'
+                  onChange={handleChange}
+                  value={model.Latitude}
+                />
+              </div>
+              <div className='flex flex-col space-y-1'>
+                <Label labelFor='Number'>Longitude</Label>
+                <Input
+                  type='text'
+                  name='Longitude'
+                  placeholder='Ex: -27.59022'
+                  className='text-black-75'
+                  onChange={handleChange}
+                  value={model.Longitude}
+                />
+              </div>
+              <div className='flex flex-col space-y-1'>
+                <Label labelFor='Number'>Number of residents</Label>
+                <Input
+                  type='text'
+                  name='Residents'
+                  placeholder='Ex: 4'
+                  className='text-black-75'
+                  onChange={handleChange}
+                  value={model.Residents}
+                />
+              </div>
+              <Button
+                type='submit'
+                className='bg-primary rounded rounded-md hover:bg-primary-hover w-full p-2'
+              >
+                <TextButton size={1} className='text-white-75'>
+                  Save
+                </TextButton>
+              </Button>
+            </form>
+          </>
+        )}
       </Box>
     </>
   )
